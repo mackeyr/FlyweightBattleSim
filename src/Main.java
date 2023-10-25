b91cfd39e925121cc0ba6ba7d289cd459c3f7504
mackeyr <mackeyr@msoe.edu>
2023-10-25 11:18:42 -0500

    Javafx main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("battle_sim.fxml"));
+        stage.setTitle("Flyweight Battle Sim");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
